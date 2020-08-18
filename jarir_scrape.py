a6d639d25a09fc82411b8b62391140b5bdbb7ea2
Omar Yassin <68425418+omaryassin96@users.noreply.github.com>
2020-08-18 16:43:01 +0300

    Update jarir_scrape.py

--- jarir_scrape.py
+++ jarir_scrape.py
@@ -1,43 +1,46 @@
+# Importing relevant libs
 import urllib.request as url
 from bs4 import BeautifulSoup as bs
 from datetime import date
 
-# Opening Today's Scraping File for Storage:
 
+# Opening Today's Scraping File for Storage:
 f = open('jarir_laptops_{}.csv'.format(date.today()), 'w')
 
-# Opening Laptops Page & Making a soup out of it:
 
+# Opening Jarir's Laptops Homepage & Making a soup out of it:
+# note: I used "req/con" lines instead of the conventional method; beacause I got a (403 Access Forbidden) error when I used the conventional code.
+# Here I plugged in jarir's laptops homepage in the variable 'u':
 u = 'https://www.jarir.com/sa-en/computers-peripherals/laptops.html'
 req = url.Request(u, headers={'User-Agent' : "Magic Browser"}) 
 con = url.urlopen(req)
 soup = bs(con.read())
 
-# Extracting each item to a list:
-# Jarir.com categorizes the items in its page into 'item last', 'item first', and 'item'.
 
+# Extracting all laptops' cards in Laptops Homepage:
+# note: Jarir.com categorizes the items in its pages into 'item last', 'item first', and 'item'.
 item_last = soup.findAll('li',{'class':'item last'})
 item_first = soup.findAll('li',{'class':'item first'})
 item = soup.findAll('li',{'class':'item'})
-
 items = item_last + item_first + item
 
-# Extracting urls from each item:
 
+# Extracting laptop url from each laptop card:
 for item in items:
     laptop_url = item.a['href']
     req = url.Request(laptop_url, headers={'User-Agent' : "Magic Browser"})
     con = url.urlopen(req)
+    # Creating a soup out of
     soup = bs(con.read())
-    
+    # Printing urls to ensure the code is running and navigating through different pages and urls
     print(laptop_url)
-    # Extracting data:
+    # Extracting data from internal tables after accessing each laptop's special page:
     data = soup.findAll('div',{'id':'card-specifications'})[0]
     rows = data.findAll('tr',{'class':'table__row'})
     for row in rows:
         f.write(row.td.text + ',')
     f.write('\n')
-    
-    # Closing Today's Scraping File:
 
+    
+# Closing Today's Scraping file:
 f.close()
